900ab9527896ad0e7f50285b4e4ec476bb8bbebd
Allison Castaneda <allraec@yahoo.com>
2021-04-13 23:13:04 -0500

    fixed server

--- api/server.js
+++ api/server.js
@@ -8,5 +8,11 @@ server.use(middleware.logger)
 // remember express by default cannot parse JSON in request bodies
 server.use(express.json())
 // global middlewares and the user's router need to be connected here
-server.use(usersRouter)
+
+server.get('/', (req, res) => {
+    res.send(`<h2>Let's write some middleware!</h2>`);
+});
+
+server.use("/api", usersRouter);
+
 module.exports = server;
